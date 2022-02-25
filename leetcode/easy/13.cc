89dfdf64247c8fd3a8da78aaa31405527a61c603
Yuri Serka <yserka@hotmail.com>
2022-02-25 18:00:27 -0300

    fix: better solutions

--- leetcode/easy/13.cc
+++ leetcode/easy/13.cc
@@ -7,36 +7,21 @@ using namespace std;
 class Solution {
  public:
   int romanToInt(string s) {
-    int r = 0;
-    for (int i = 0, l = s.size(); i < l; ++i) {
-      char c = s[i];
-      switch (c) {
-        case 'I':
-          r += getvalpeek(s, i + 1 < l, c, 'V', 'X', &i);
-          break;
-        case 'X':
-          r += getvalpeek(s, i + 1 < l, c, 'L', 'C', &i);
-          break;
-        case 'C':
-          r += getvalpeek(s, i + 1 < l, c, 'D', 'M', &i);
-          break;
-        default:
-          r += getvalpeek(s, false, c, '\0', '\0', &i);
-          break;
-      }
+    const int len = s.size();
+
+    if (!len) return 0;
+
+    unordered_map<char, int> m = {{'I', 1},   {'V', 5},   {'X', 10},  {'L', 50},
+                                  {'C', 100}, {'D', 500}, {'M', 1000}};
+
+    long long ans = 0;
+    for (int i = 0; i < len - 1; ++i) {
+      int curr = m[s[i]];
+      int nxt = m[s[i + 1]];
+      ans += curr < nxt ? -curr : curr;
     }
-    return r;
-  }
+    ans += m[s.back()];
 
-  int getvalpeek(const string &s, bool canpeek, char curr, char b41, char b42,
-                 int *i) {
-    if (!canpeek) return tb[curr];
-    char peek = s[*i + 1];
-    return peek == b41 || peek == b42 ? (++*i, tb[peek] - tb[curr]) : tb[curr];
+    return ans;
   }
-
-  map<char, int> tb = {
-      {'I', 1},   {'V', 5},   {'X', 10},   {'L', 50},
-      {'C', 100}, {'D', 500}, {'M', 1000},
-  };
 };
--- leetcode/easy/9.cc
+++ leetcode/easy/9.cc
@@ -8,6 +8,23 @@ class Solution {
  public:
   bool isPalindrome(int x) {
     if (x < 0) return false;
+    if (!x) return true;
+    if (!(x % 10)) return false;
+
+    long long sum = 0;
+    while (x > sum) {
+      sum = (sum * 10) + (x % 10);
+      x /= 10;
+    }
+
+    return (x == sum || x == sum / 10);
+  }
+};
+
+class TrickySolution {
+ public:
+  bool isPalindrome(int x) {
+    if (x < 0) return false;
 
     auto s = to_string(x);
 
