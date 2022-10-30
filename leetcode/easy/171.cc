e90fd7d2542c67daf91439f884013175f0acf468
Yuri Serka <yserka@hotmail.com>
2022-10-29 22:10:40 -0300

    fix: no need for the if stmt

--- leetcode/easy/171.cc
+++ leetcode/easy/171.cc
@@ -12,8 +12,6 @@ class Solution {
     int ans = 0;
     int len = columnTitle.size();
 
-    if (len < 2) return columnTitle[0] - OFFSET;
-
     for (int i = len - 1, j = 0; i >= 0; --i, ++j) {
       ans += pow(26, i) * (columnTitle[j] - OFFSET);
     }
